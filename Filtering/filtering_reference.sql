83baeaf0c495ae6b9307b792e9ccbc0a5904dd9c
Bandeep Bhatta <bandeepbhatta@gmail.com>
2026-04-22 18:20:15 +0530

    Create filtering_reference.sql

---
+++ Filtering/filtering_reference.sql
@@ -0,0 +1,40 @@
+﻿-- ============================================
+-- 📌 SQL PRACTICE QUESTIONS (FILTERING - DQL)
+-- ============================================
+
+-- 🗂️ TABLE: Employees
+
+CREATE TABLE Employees (
+emp_id INT PRIMARY KEY,
+name VARCHAR(50),
+department VARCHAR(50),
+salary DECIMAL(10,2),
+city VARCHAR(50)
+);
+
+-- ============================================
+-- 📊 SUMMARY
+-- ============================================
+
+-- Total Questions Practiced: 13
+
+-- Topics Covered:
+-- WHERE clause
+-- Comparison operators
+-- AND, OR, NOT
+-- IN
+-- BETWEEN
+-- LIKE
+-- IS NULL / IS NOT NULL
+-- ORDER BY
+
+-- Key Learnings:
+-- 1. WHERE clause is essential for filtering data
+-- 2. Logical operators control complex conditions
+-- 3. LIKE is used for pattern matching
+-- 4. BETWEEN handles ranges
+-- 5. NULL requires special handling (IS NULL)
+
+-- ============================================
+-- END OF NOTES
+-- ============================================
